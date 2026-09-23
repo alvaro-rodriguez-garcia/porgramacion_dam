e9649739a8a1d520e747be5bef5e485f0c38e637
alvaro-rodriguez-garcia <alvaro30798@gmail.com>
2026-09-23 12:15:35 +0200

    update

--- Componentes/src/Entrada.java
+++ Componentes/src/Entrada.java
@@ -1,3 +1,38 @@
 public class Entrada {
-    //hola alvaroo
+
+    public static void main (String[] args) {
+
+        //variable:
+            //segun el dato que guarda: String,char,byte/shot/long/int,double/float,boolean
+
+        //para definirla
+            //tipo nombre = valor
+
+        //para cambiar el valor del string no poner otra vez string porque seria otra variable
+
+        String nombreLegal = "Borja";
+        nombreLegal = "Borja M";
+
+        char letra = 'a';
+
+        int edad = 41;
+
+        double altura = 1.74;
+
+        //float alturaFloat = 1.74f esto es para no liarnos porque ahora vamos a trabajar con double
+
+        boolean acierto = true;
+
+        System.out.println("El resultado de la evaluacion es "+acierto);
+        System.out.println("La letra de mi Dni es "+letra);
+        System.out.println("Mi nombre es "+nombreLegal);
+        String nombre = "null";
+        System.out.println("Hola Mundo");
+        System.out.println("Segunda linea");
+        System.out.println(nombre.length());
+        System.out.println(9);
+        System.out.println(9+5);
+        System.out.println("La suma de "+9+ " y " +6+ " da como resultado "+ (9+6));
+    }
 }
+
