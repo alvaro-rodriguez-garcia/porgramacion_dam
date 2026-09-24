3016068d9f8a8c3bc0e551bfcbffbdb2af3281fe
alvaro-rodriguez-garcia <alvaro30798@gmail.com>
2026-09-24 10:51:13 +0200

    update

--- Componentes/src/Entrada.java
+++ Componentes/src/Entrada.java
@@ -4,6 +4,9 @@ public class Entrada {
 
         //variable:
             //segun el dato que guarda: String,char,byte/shot/long/int,double/float,boolean
+            //segun la forma en construirse: primitivos(solo guarda el valor) o complejos(ademas del valor se guarda una funcionalidad)
+            //segun la mutabilidad del dato: mutables o no mutables (constante, todas en mayuscula)
+            //segun el scope de la variable: metodo (bloque) / clase
 
         //para definirla
             //tipo nombre = valor
@@ -13,15 +16,38 @@ public class Entrada {
         String nombreLegal = "Borja";
         nombreLegal = "Borja M";
 
+        //Vamos a ver como hacer un primitivo que sea complejo
+
+
         char letra = 'a';
+        Character letraCompleja = 'a'; //ahora es complejo
 
         int edad = 41;
+        Integer edadCompleja = 41; //ahora es complejo
 
         double altura = 1.74;
+        Double alturaCompleja = 1.74; //ahora es complejo
 
         //float alturaFloat = 1.74f esto es para no liarnos porque ahora vamos a trabajar con double
 
         boolean acierto = true;
+        Boolean aciertoComplejo = true; //ahora es complejo
+
+
+        //por ejemplo vamos hacer una funcionalidad a un complejo
+        nombreLegal.length();//te dice la longitud
+
+        //vamos a ver una variables no mutable
+        final String DNI = "123A";
+        //dni= "12345A"; da error porque al poner final se le dice al programa que no se puede cambiar
+
+        //segun el scope , metodo
+        //se puede utilizar la variable porque esta definida dentro del metodo si estuviese fuera no la podria utlizar
+        //si estuviese dentro de otro metodo no podria utilizarla
+        //si yo defino la variable en el metodo mas grande si la podria utilizar para todos os metodos que estan dentro del metodo mas grande
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------
+
 
         System.out.println("El resultado de la evaluacion es "+acierto);
         System.out.println("La letra de mi Dni es "+letra);
@@ -33,6 +59,11 @@ public class Entrada {
         System.out.println(9);
         System.out.println(9+5);
         System.out.println("La suma de "+9+ " y " +6+ " da como resultado "+ (9+6));
+
+
+
+
+
     }
 }
 
